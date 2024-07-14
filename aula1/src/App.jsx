885e09717d40538f060f3a64559e3f264c2f6695
RodRibDev <rodrigo_r_ribeiro@estudante.sesisenai.org.br>
2024-07-14 10:02:20 -0300

    Resolução exercício 03

--- aula1/src/App.jsx
+++ aula1/src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Contador from "./components/contador";
+import Lista from "./components/lista";
 import Noticia from "./components/noticia";
 
 export default function App() {
   return <><Contador></Contador>
+  <hr className="separator" />
   <Noticia></Noticia>
+  <hr className="separator" />
+  <Lista></Lista>
   </>
 
 }
